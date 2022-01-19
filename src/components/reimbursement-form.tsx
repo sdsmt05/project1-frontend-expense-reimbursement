3858f39591100d29b94ca5364b7d22c0979c88d1
Brandon Zander <bzander95@hotmail.com>
2022-01-18 20:46:39 -0700

    Add negative number handling

--- src/components/reimbursement-form.tsx
+++ src/components/reimbursement-form.tsx
@@ -19,10 +19,14 @@ export default function ReimbursementForm(props: {reimbursements: Reimbursement[
             isApproved: IsApproved.pending
         }
 
+
         if(!reimbursement.amount || !reimbursement.reason){
             alert("Invalid submission: Either the Amount or Reason is missing");
-    
-        } else {
+        } else if(reimbursement.amount <= 0){
+            alert("Amount should be a positive number.");
+            amountInput.current.value = "";
+        }  else {
+
             const response = await fetch('http://localhost:5000/reimbursements', {
                 method: 'POST',
                 body: JSON.stringify(reimbursement),
@@ -37,17 +41,16 @@ export default function ReimbursementForm(props: {reimbursements: Reimbursement[
             } else {
                 alert("THERE WAS AN ERROR SUBMITTING THE REIMBURSEMENT");
             }
+            amountInput.current.value = "";
+            reasonInput.current.value = "";
         }
-
-        amountInput.current.value = "";
-        reasonInput.current.value = "";
     }
 
     return(<>
         <h3 style={{color: "#606c76"}}>Reimbursement Submission Form</h3>
 
         <label htmlFor="amountInput">Amount</label>
-        <input ref={amountInput} type="number" id="amountInput" placeholder="50"/>
+        <input ref={amountInput} type="number" id="amountInput" placeholder="50" min={0}/>
         <label htmlFor="reasonInput">Reason</label>
         <input ref={reasonInput} type="text" id="reasonInput" placeholder="Gas"/>
 
